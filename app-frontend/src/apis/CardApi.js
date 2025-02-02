5b9c7152428409338e2e6cf88ec335f30549ca58
Hyukjoo-Lee <hjjaydon.lee@gmail.com>
2025-02-03 02:15:18 +0900

    Feat: 카드 등록 프론트 구현

--- app-frontend/src/apis/CardApi.js
+++ app-frontend/src/apis/CardApi.js
@@ -7,3 +7,19 @@ export const fetchSearchCard = async (keyword) => {
   });
   return response.data;
 };
+
+// 카드 생성
+export const createCard = async (cardInfo) => {
+  try {
+    const response = await axios.post(`/card/create`, cardInfo, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    }
+  }
+};
